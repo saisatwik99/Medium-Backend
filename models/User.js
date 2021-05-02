25f48e5f36f43b9b81a7a6055175b85f271c27e9
saisatwik99 <saisatwik99@gmail.com>
2021-05-02 12:27:03 +0530

    Fixed user schema

--- models/User.js
+++ models/User.js
@@ -4,10 +4,6 @@ let UserSchema = new mongoose.Schema(
     {
         name: String,
         email: String,
-        provider: String,
-        provider_id: String,
-        token: String,
-        provider_pic: String,
         followers: [
             {
                 type: mongoose.Schema.Types.ObjectId,
